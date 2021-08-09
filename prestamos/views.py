aeac58ad165d0d72b15d173eac895917152bba76
Marco Caballero <mcaballeroguillen@gmail.com>
2021-08-08 20:09:26 -0600

    error mostrado

--- prestamos/views.py
+++ prestamos/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django import forms
 from django.views.generic import TemplateView, RedirectView
 # Create your views here.
 
 def Prestamos(request):
-    if request.method=="post":
-
-        return render(request, "transactions/Prestamos.html")
+    if request.method=="POST":
+        id = request.POST['Identidad']
+        if len(id)!=13:
+            messages.error(request,"Error en Identidad","Debe medir 14")
+        #return render(request, "transactions/Prestamos.html")
 
 
 
